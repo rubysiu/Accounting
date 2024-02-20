52de5cf6c989c66e5057c0222e46b6c0678df494
ruby <rubysiu@163.com>
2024-02-20 13:54:25 +0800

    init

---
+++ src/main/java/com/ruby/project/system/controller/SysRegisterController.java
@@ -0,0 +1,38 @@
+package com.ruby.project.system.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.ruby.common.utils.StringUtils;
+import com.ruby.framework.security.RegisterBody;
+import com.ruby.framework.security.service.SysRegisterService;
+import com.ruby.framework.web.controller.BaseController;
+import com.ruby.framework.web.domain.AjaxResult;
+import com.ruby.project.system.service.ISysConfigService;
+
+/**
+ * 注册验证
+ * 
+ * @author ruoyi
+ */
+@RestController
+public class SysRegisterController extends BaseController
+{
+    @Autowired
+    private SysRegisterService registerService;
+
+    @Autowired
+    private ISysConfigService configService;
+
+    @PostMapping("/register")
+    public AjaxResult register(@RequestBody RegisterBody user)
+    {
+        if (!("true".equals(configService.selectConfigByKey("sys.account.registerUser"))))
+        {
+            return error("当前系统没有开启注册功能！");
+        }
+        String msg = registerService.register(user);
+        return StringUtils.isEmpty(msg) ? success() : error(msg);
+    }
+}
